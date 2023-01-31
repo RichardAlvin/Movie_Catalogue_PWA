12deab12c36d5e8e977ebeccf4a6760847330739
Richard Alvin <richardalvinpratama8@gmail.com>
2023-01-31 10:38:45 +0700

    finish get data and design

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    KEY: 'b6fd205761475b546ea6858bbe273a71',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
